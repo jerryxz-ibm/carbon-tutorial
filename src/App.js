422024f19ee3238bd3efd31d6808c9eda987d90d
jerryxz-ibm <>
2021-01-07 10:55:46 -0500

    feat(tutorial): complete step 1 - dummy test

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// Dummy modification for Git test
+
 class App extends Component {
   render() {
     return (
